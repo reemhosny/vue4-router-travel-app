01f11ae78579b9265771db7252c0b5be25cb1047
reemhosny <reem.hosny@waffarha.com>
2024-10-17 10:06:30 +0300

    feat: :truck: creat countries files and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home,
     },
     {
       path: '/about',
@@ -15,9 +15,29 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import('../views/About.vue'),
+    },
+    {
+      path: '/brazil',
+      name: 'brazil',
+      component: () => import('../views/Brazil.vue'),
+    },
+    {
+      path: '/hawaii',
+      name: 'hawaii',
+      component: () => import('../views/Hawaii.vue'),
+    },
+    {
+      path: '/panama',
+      name: 'panama',
+      component: () => import('../views/Panama.vue'),
+    },
+    {
+      path: '/jamaica',
+      name: 'jamaica',
+      component: () => import('../views/Jamaica.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/Hawaii.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="hawaii">
+    <h1>Hawaii</h1>
+  </div>
+</template>
+
+<style></style>
